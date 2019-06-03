7731c25935c4f64ab1b7c367a71ad61009d47436
btierra <49373949+btierra@users.noreply.github.com>
2019-06-03 11:23:49 -0700

    K channels open as a function of v and temp
    accounts for height change of action potential

--- alphan.m
+++ alphan.m
@@ -1,8 +1,9 @@
 function a=alphan(v)
 %filename: alphan.m
+global temp
 theta=(v+60)/10;
 if(theta==0)   %check for case that gives 0/0
   a=0.1;  %in that case use L'Hospital's rule
 else
-  a=0.1*theta/(1-exp(-theta));
+  a=((3^((temp-37)/10))*0.1*theta)/(1-exp(-theta));
 end
