946bddee3922580e2a5cd4a45b2a171bf1d19b23
btierra <49373949+btierra@users.noreply.github.com>
2019-06-02 22:25:42 -0700

    accounts for temp change

--- alpham.m
+++ alpham.m
@@ -1,9 +1,9 @@
 function a=alpham(v)
 %filename: alpham.m
-global t 
+global temp
 theta=(v+45)/10;
 if(theta==0)   %check for case that gives 0/0
   a=1.0;  %in that case use L'Hospital's rule
 else
-  a=1.0*theta/(1-exp(-theta));
+  a=(0.017*exp(0.11*temp))*1.0*theta/(1-exp(-theta));
 end
