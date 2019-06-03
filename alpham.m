f9d3896931997daced95a1afb19c8ef544f3945f
btierra <49373949+btierra@users.noreply.github.com>
2019-06-03 11:04:31 -0700

    simplify scaling factor of temp

--- alpham.m
+++ alpham.m
@@ -5,5 +5,5 @@ theta=(v+45)/10;
 if(theta==0)   %check for case that gives 0/0
   a=1.0;  %in that case use L'Hospital's rule
 else
-  a=(0.017*exp(0.11*temp))*1.0*theta/(1-exp(-theta));
+  a=((3^((temp-37)/10))*1.0*theta)/(1-exp(-theta));
 end
